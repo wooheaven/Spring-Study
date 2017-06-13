ec494d10767ff450c949b70efe508ebd5b9a1978
wooheaven <wooheaven79@gmail.com>
2017-06-14 02:54:41 +0900

    #1-inflearn-spring-lecture-1-and-2 새 파일:       .gitignore 새 파일:       01_Spring_Workspace/Servers/Pivotal tc Server Developer Edition v3.2-config/catalina.policy 새 파일:       01_Spring_Workspace/Servers/Pivotal tc Server Developer Edition v3.2-config/catalina.properties 새 파일:       01_Spring_Workspace/Servers/Pivotal tc Server Developer Edition v3.2-config/context.xml 새 파일:       01_Spring_Workspace/Servers/Pivotal tc Server Developer Edition v3.2-config/jmxremote.access 새 파일:       01_Spring_Workspace/Servers/Pivotal tc Server Developer Edition v3.2-config/jmxremote.password 새 파일:       01_Spring_Workspace/Servers/Pivotal tc Server Developer Edition v3.2-config/server.xml 새 파일:       01_Spring_Workspace/Servers/Pivotal tc Server Developer Edition v3.2-config/tomcat-users.xml 새 파일:       01_Spring_Workspace/Servers/Pivotal tc Server Developer Edition v3.2-config/web.xml 새 파일:       01_Spring_Workspace/Servers/Tomcat v8.5 Server at localhost-config/catalina.policy 새 파일:       01_Spring_Workspace/Servers/Tomcat v8.5 Server at localhost-config/catalina.properties 새 파일:       01_Spring_Workspace/Servers/Tomcat v8.5 Server at localhost-config/context.xml 새 파일:       01_Spring_Workspace/Servers/Tomcat v8.5 Server at localhost-config/server.xml 새 파일:       01_Spring_Workspace/Servers/Tomcat v8.5 Server at localhost-config/tomcat-users.xml 새 파일:       01_Spring_Workspace/Servers/Tomcat v8.5 Server at localhost-config/web.xml 새 파일:       01_Spring_Workspace/firstEx/pom.xml 새 파일:       01_Spring_Workspace/firstEx/src/main/java/com/spring/example/A.java 새 파일:       01_Spring_Workspace/firstEx/src/main/java/com/spring/example/B.java 수정함:        README.md

---
+++ 01_Spring_Workspace/firstEx/src/main/java/com/spring/example/A.java
@@ -0,0 +1,10 @@
+package com.spring.example;
+
+public class A {
+	public static void main(String[] args) {
+		B b = new B();
+		b.setFirstNum(10);
+		b.setSecondNum(2);
+		b.add();
+	}
+}
---
+++ 01_Spring_Workspace/firstEx/src/main/java/com/spring/example/B.java
@@ -0,0 +1,23 @@
+package com.spring.example;
+
+public class B {
+	private int firstNum;
+	private int secondNum;
+	
+	public int getFirstNum() {
+		return firstNum;
+	}
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+	public int getSecondNum() {
+		return secondNum;
+	}
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+	
+	public void add() {
+		System.out.println(firstNum + secondNum);
+	}
+}
