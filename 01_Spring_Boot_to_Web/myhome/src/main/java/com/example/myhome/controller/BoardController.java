3566e45106db3ddb6348003f1b272e3040bc1a10
WooRung <wooheaven79@gmail.com>
2021-12-03 22:37:33 +0900

    #20-spring-boot-to-web-4-boards-table-view ``` modified:   01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/controller/BoardController.java modified:   01_Spring_Boot_to_Web/myhome/src/main/resources/templates/board/list.html ```

--- 01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/controller/BoardController.java
+++ 01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.myhome.controller;
 import com.example.myhome.model.Board;
 import com.example.myhome.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +20,7 @@ public class BoardController {
 
     @GetMapping("/list")
     public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+        List<Board> boards = boardRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
         model.addAttribute("boards", boards);
         return "board/list";
     }
