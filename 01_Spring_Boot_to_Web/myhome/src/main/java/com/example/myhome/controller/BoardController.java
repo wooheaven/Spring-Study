990c9afe9b1318c6133aba6202488d7400ddc1bd
WooRung <wooheaven79@gmail.com>
2021-12-04 13:07:21 +0900

    #21-spring-boot-to-web-6-writing-on-board ``` modified:   01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/controller/BoardController.java new file:   01_Spring_Boot_to_Web/myhome/src/main/resources/templates/board/form.html modified:   01_Spring_Boot_to_Web/myhome/src/main/resources/templates/board/list.html ```

--- 01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/controller/BoardController.java
+++ 01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/controller/BoardController.java
@@ -2,11 +2,14 @@ package com.example.myhome.controller;
 
 import com.example.myhome.model.Board;
 import com.example.myhome.repository.BoardRepository;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,5 +26,17 @@ public class BoardController {
         List<Board> boards = boardRepository.findAll(Sort.by(Sort.Direction.ASC, "id"));
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    @GetMapping("/form")
+    public String boardForm(Model model) {
+        model.addAttribute("board", new Board());
+        return "board/form";
+    }
+
+    @PostMapping("/form")
+    public String boardSubmit(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
