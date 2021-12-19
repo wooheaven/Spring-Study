4aeb1a2154e85e27de97390f6762a6f4018c86f9
WooRung <wooheaven79@gmail.com>
2021-12-19 21:51:05 +0900

    #26-spring-boot-to-web-7-rest-full-api-by-jpa ``` new file:   01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/controller/BoardApiController.java modified:   01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/model/Board.java modified:   01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/repository/BoardRepository.java ```

--- 01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/repository/BoardRepository.java
+++ 01_Spring_Boot_to_Web/myhome/src/main/java/com/example/myhome/repository/BoardRepository.java
@@ -3,5 +3,8 @@ package com.example.myhome.repository;
 import com.example.myhome.model.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends JpaRepository<Board, Long> {
+    List<Board> findAllByTitleContainsAndContentContainsOrderByIdAsc(String title, String content);
 }
