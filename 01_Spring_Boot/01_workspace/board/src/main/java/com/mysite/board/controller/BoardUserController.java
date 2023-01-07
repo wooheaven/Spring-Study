de4724ed8b9bef6b2d7b7ded05bbb930288eb11e
WooRung <wooheaven79@gmail.com>
2023-01-07 14:54:52 +0900

    #56-duplicated-boarduser ``` modified:   01_Spring_Boot/01_workspace/board/src/main/java/com/mysite/board/controller/BoardUserController.java ```

--- 01_Spring_Boot/01_workspace/board/src/main/java/com/mysite/board/controller/BoardUserController.java
+++ 01_Spring_Boot/01_workspace/board/src/main/java/com/mysite/board/controller/BoardUserController.java
@@ -1,5 +1,6 @@
 package com.mysite.board.controller;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -34,7 +35,18 @@ public class BoardUserController {
             return "signup_form";
         }
 
-        boardUserService.create(boardUserForm.getUsername(), boardUserForm.getEmail(), boardUserForm.getPassword1());
+        try {
+            boardUserService.create(boardUserForm.getUsername(), boardUserForm.getEmail(),
+                    boardUserForm.getPassword1());
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+            return "signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "signup_form";
+        }
 
         return "redirect:/";
     }
