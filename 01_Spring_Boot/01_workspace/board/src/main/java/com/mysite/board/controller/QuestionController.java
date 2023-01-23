6eeee547ea6c77b492c5b3fe2e61f61e54159cd1
WooRung <wooheaven79@gmail.com>
2023-01-23 17:36:46 +0900

    #61-delete-question ``` modified:   01_Spring_Boot/01_workspace/board/src/main/java/com/mysite/board/controller/QuestionController.java modified:   01_Spring_Boot/01_workspace/board/src/main/java/com/mysite/board/service/QuestionService.java modified:   01_Spring_Boot/01_workspace/board/src/main/resources/application.properties modified:   01_Spring_Boot/01_workspace/board/src/main/resources/templates/layout.html modified:   01_Spring_Boot/01_workspace/board/src/main/resources/templates/question_detail.html ```

--- 01_Spring_Boot/01_workspace/board/src/main/java/com/mysite/board/controller/QuestionController.java
+++ 01_Spring_Boot/01_workspace/board/src/main/java/com/mysite/board/controller/QuestionController.java
@@ -90,4 +90,15 @@ public class QuestionController {
         this.questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
         return String.format("redirect:/question/detail/%s", id);
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String questionDelete(Principal principal, @PathVariable("id") Long id) {
+        Question question = this.questionService.getQuestion(id);
+        if (!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
+        this.questionService.delete(question);
+        return "redirect:/question/list"; // 질문 삭제 후 질문목록으로 이동
+    }
 }
