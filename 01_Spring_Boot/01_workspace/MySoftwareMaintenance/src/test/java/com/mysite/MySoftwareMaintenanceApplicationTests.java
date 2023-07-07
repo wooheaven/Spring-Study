d3d19627ca13edd9671d68c0e4e1eceed7a5fe4e
WooRung <wooheaven79@gmail.com>
2023-07-07 14:44:41 +0000

    #87-add-brewupdaterepository-save-and-findall-test ``` Add Test : Add test for brewUpdateRepository's save and findAll to cover the function save and findAll of brewUpdateRepository ```

--- 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/test/java/com/mysite/MySoftwareMaintenanceApplicationTests.java
+++ 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/test/java/com/mysite/MySoftwareMaintenanceApplicationTests.java
@@ -1,13 +1,33 @@
 package com.mysite;
 
+import com.mysite.brew.model.BrewUpdate;
+import com.mysite.brew.repository.BrewUpdateRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class MySoftwareMaintenanceApplicationTests {
+    @Autowired
+    private BrewUpdateRepository brewUpdateRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assert brewUpdateRepository != null;
+    }
 
+    @Test
+    void brewUpdateRepository_save_findAll_test() {
+        String content = "Already up-to-date";
+        BrewUpdate brewUpdate = new BrewUpdate();
+        brewUpdate.setContent(content);
+        brewUpdateRepository.save(brewUpdate);
+
+        List<BrewUpdate> brewUpdateList = brewUpdateRepository.findAll();
+
+        assert 1 == brewUpdateList.size();
+        assert content == brewUpdateList.get(0).getContent();
+    }
 }
