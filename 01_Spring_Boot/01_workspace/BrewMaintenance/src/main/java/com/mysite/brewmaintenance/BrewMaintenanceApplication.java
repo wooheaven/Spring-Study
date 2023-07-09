1268cd58ce0530af839fd561779d641719e13124
WooRung <wooheaven79@gmail.com>
2023-07-09 08:24:30 +0000

    #88-replace-robot-to-terminal-stream-callable ``` Add Submodule : Add brew project Migrate brew from MySoftwareMaintenance ```

---
+++ 01_Spring_Boot/01_workspace/BrewMaintenance/src/main/java/com/mysite/brewmaintenance/BrewMaintenanceApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.brewmaintenance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewMaintenanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewMaintenanceApplication.class, args);
+	}
+
+}
---
+++ 01_Spring_Boot/01_workspace/BrewMaintenance/src/test/java/com/mysite/brewmaintenance/BrewMaintenanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.brewmaintenance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewMaintenanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
