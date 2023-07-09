f408b36e846699d652b4192547f372050f7c7612
WooRung <wooheaven79@gmail.com>
2023-07-09 16:39:44 +0000

    #88-replace-robot-to-terminal-stream-callable ``` New Feature : test for BrewService's update method To check if the method is working ```

--- 01_Spring_Boot/01_workspace/BrewMaintenance/src/test/java/com/mysite/brew/BrewMaintenanceApplicationTests.java
+++ 01_Spring_Boot/01_workspace/BrewMaintenance/src/test/java/com/mysite/brew/BrewMaintenanceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.mysite.brew;
 
+import com.mysite.brew.model.BrewUpdate;
+import com.mysite.brew.repository.BrewUpdateRepository;
+import com.mysite.brew.service.BrewService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class BrewMaintenanceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private BrewUpdateRepository brewUpdateRepository;
+    @Autowired
+    private BrewService brewService;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void BrewService_update_test() throws Exception {
+        long before = this.brewUpdateRepository.count();
+        assert 0 == before;
+        this.brewService.update();
+        long after = this.brewUpdateRepository.count();
+        assert 1 == after;
+        Optional<BrewUpdate> optional = brewUpdateRepository.findById(after);
+        String content = "";
+        if (optional.isPresent()) {
+            content = optional.get().getContent();
+        }
+        assert 0 < content.length();
+    }
 }
