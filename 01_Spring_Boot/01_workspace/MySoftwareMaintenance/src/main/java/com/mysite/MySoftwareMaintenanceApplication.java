f7563b27357799057f3992b95533fd00b452df48
WooRung <wooheaven79@gmail.com>
2023-07-09 16:13:23 +0000

    #88-replace-robot-to-terminal-stream-callable ``` ETC ```

--- 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/java/com/mysite/MySoftwareMaintenanceApplication.java
+++ 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/java/com/mysite/MySoftwareMaintenanceApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
 @EnableJpaAuditing
+@SpringBootApplication
 public class MySoftwareMaintenanceApplication {
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MySoftwareMaintenanceApplication.class, args);
     }
