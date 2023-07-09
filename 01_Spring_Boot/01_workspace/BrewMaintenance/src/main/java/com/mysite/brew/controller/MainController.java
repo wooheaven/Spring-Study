8e4825caf9e282bc26c26a8d58ba49688fcfada8
WooRung <wooheaven79@gmail.com>
2023-07-09 16:02:52 +0000

    #88-replace-robot-to-terminal-stream-callable ``` New Feature : Mapping / to hello To check if the web module is working ```

---
+++ 01_Spring_Boot/01_workspace/BrewMaintenance/src/main/java/com/mysite/brew/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.mysite.brew.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String root() {
+        return "hello";
+    }
+}
--- 01_Spring_Boot/01_workspace/BrewMaintenance/src/test/java/com/mysite/brew/BrewMaintenanceApplicationTests.java
+++ 01_Spring_Boot/01_workspace/BrewMaintenance/src/test/java/com/mysite/brew/BrewMaintenanceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mysite.brewmaintenance;
+package com.mysite.brew;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
