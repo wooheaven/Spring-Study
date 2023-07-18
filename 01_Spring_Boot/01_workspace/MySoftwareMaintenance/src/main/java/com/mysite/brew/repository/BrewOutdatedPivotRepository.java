e9e4dcb6feecd0e837a16a17ec8b8967bf895a28
WooRung <wooheaven79@gmail.com>
2023-07-18 20:21:13 +0000

    #99-remove-brew ``` ETC : remove brew from My Since the migration is finished (brew) ```

--- 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/java/com/mysite/brew/repository/BrewOutdatedPivotRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mysite.brew.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mysite.brew.model.BrewOutdatedPivot;
-
-public interface BrewOutdatedPivotRepository extends JpaRepository<BrewOutdatedPivot, Long> {
-
-}
