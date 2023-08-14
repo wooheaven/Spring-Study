272723bb885b619b048dd86c641c0c6b3bd4471c
WooRung <wooheaven79@gmail.com>
2023-08-14 23:46:00 +0000

    #110-modify-time-on-icloud ``` ETC : modify time To controll icloud ```

--- 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/java/com/mysite/icloud/controller/IcloudController.java
+++ 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/java/com/mysite/icloud/controller/IcloudController.java
@@ -15,43 +15,46 @@ public class IcloudController {
     @GetMapping("/moveFile")
     public String moveFile() throws AWTException {
         MyRobot myRobot = new MyRobot();
+        moveFile(myRobot);
+        goToBase(myRobot);
+        return "redirect:/";
+    }
+
+    private void moveFile(MyRobot myRobot) throws AWTException {
         myRobot.mouseMove(500, 1350, 400);
         myRobot.mouseLeftClick(100);
         myRobot.mouseMove(500, 1790, 185);
+        myRobot.mouseLeftClick(2500);
+        myRobot.altS(1100);
+    }
+
+    private void goToBase(MyRobot myRobot) throws AWTException {
+        myRobot.mouseMove(500, 790, 185);
         myRobot.mouseLeftClick(100);
-        myRobot.altS(1000);
-        return "redirect:/";
     }
 
     @GetMapping("/trash")
     public String trash() throws AWTException {
         MyRobot myRobot = new MyRobot();
+        trash(myRobot);
+        goToBase(myRobot);
+        return "redirect:/";
+    }
+
+    private void trash(MyRobot myRobot) throws AWTException {
         myRobot.mouseMove(500, 1350, 400);
         myRobot.mouseLeftClick(100);
         myRobot.mouseMove(500, 1835, 185);
         myRobot.mouseLeftClick(100);
         myRobot.keyboardPressRelease(100, "");
-        return "redirect:/";
     }
 
     @GetMapping("/etc")
     public String etc() throws AWTException {
         MyRobot myRobot = new MyRobot();
-        /* moveFile */
-        myRobot.mouseMove(500, 1350, 400);
-        myRobot.mouseLeftClick(100);
-        myRobot.mouseMove(500, 1790, 185);
-        myRobot.mouseLeftClick(100);
-        myRobot.altS(1000);
-        /* trash */
-        myRobot.mouseMove(500, 1350, 400);
-        myRobot.mouseLeftClick(100);
-        myRobot.mouseMove(500, 1835, 185);
-        myRobot.mouseLeftClick(100);
-        myRobot.keyboardPressRelease(100, "");
-        /* click left */
-        myRobot.mouseMove(500, 500, 500);
-        myRobot.mouseLeftClick(100);
+        moveFile(myRobot);
+        trash(myRobot);
+        goToBase(myRobot);
         return "redirect:/";
     }
 }
