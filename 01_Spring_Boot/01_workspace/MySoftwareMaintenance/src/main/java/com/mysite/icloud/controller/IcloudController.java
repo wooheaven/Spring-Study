a844c32895f7b185bf087e3614f3ea40f46356a8
WooRung <wooheaven79@gmail.com>
2024-02-25 15:15:12 +0000

    #134-update-icloud-controller ``` modified:   01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/java/com/mysite/icloud/controller/IcloudController.java deleted:    01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/resources/application-prod.properties modified:   01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/resources/application.properties modified:   01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/resources/templates/index.html ```

--- 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/java/com/mysite/icloud/controller/IcloudController.java
+++ 01_Spring_Boot/01_workspace/MySoftwareMaintenance/src/main/java/com/mysite/icloud/controller/IcloudController.java
@@ -2,15 +2,19 @@ package com.mysite.icloud.controller;
 
 import java.awt.AWTException;
 
+import com.mysite.robot.MyRobot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.mysite.robot.MyRobot;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/icloud")
 @Controller
 public class IcloudController {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/moveFile")
     public String moveFile() throws AWTException {
@@ -22,19 +26,26 @@ public class IcloudController {
 
     private void moveFile(MyRobot myRobot) throws AWTException {
         int delay = 850;
-        myRobot.mouseMove(delay, 420, 350);
+        int firstPictureX = 430;
+        int firstPictureY = 380;
+        int secondDownloadX = firstPictureX + 30;
+        int secondDownloadY = firstPictureY + 65;
+        int thirdDownloadY = firstPictureY + 95;
+        // Download
+        myRobot.mouseMove(delay, firstPictureX, firstPictureY);
         myRobot.mouseLeftClick(delay);
         myRobot.mouseRightClick(delay);
-        myRobot.mouseMove(delay, 450, 410);
+        myRobot.mouseMove(delay, secondDownloadX, secondDownloadY);
         myRobot.mouseLeftClick(delay * 4);
         myRobot.altS(delay * 4);
 
-        myRobot.mouseMove(delay, 420, 350);
+        // More Download Options
+        myRobot.mouseMove(delay, firstPictureX, firstPictureY);
         myRobot.mouseLeftClick(delay);
         myRobot.mouseRightClick(delay);
-        myRobot.mouseMove(delay, 450, 440);
+        myRobot.mouseMove(delay, secondDownloadX, thirdDownloadY);
         myRobot.mouseLeftClick(delay);
-        myRobot.mouseMove(delay, 1160, 650);
+        myRobot.mouseMove(delay, 1160, 670);
         myRobot.mouseLeftClick(delay * 5);
         myRobot.altS(delay * 6);
     }
@@ -54,11 +65,15 @@ public class IcloudController {
     }
 
     private void trash(MyRobot myRobot) throws AWTException {
-        int delay = 700;
-        myRobot.mouseMove(delay, 420, 350);
+        int delay = 850;
+        int firstPictureX = 430;
+        int firstPictureY = 380;
+        int secondPictureX = firstPictureX + 30;
+        int secondPictureY = firstPictureY + 265;
+        myRobot.mouseMove(delay, firstPictureX, firstPictureY);
         myRobot.mouseLeftClick(delay);
         myRobot.mouseRightClick(delay);
-        myRobot.mouseMove(delay, 450, 610);
+        myRobot.mouseMove(delay, secondPictureX, secondPictureY);
         myRobot.mouseLeftClick(delay);
         myRobot.mouseMove(delay, 1090, 740);
         myRobot.mouseLeftClick(delay);
@@ -66,10 +81,15 @@ public class IcloudController {
     }
 
     @GetMapping("/etc")
-    public String etc() throws AWTException {
-        MyRobot myRobot = new MyRobot();
-        moveFile(myRobot);
-        trash(myRobot);
+    public String etc(@RequestParam(value = "count", defaultValue = "1") Integer count) throws AWTException {
+        logger.info("count " + count + " is requested");
+        for (int i = 1; i <= count; i++) {
+            logger.info("current count is " + i);
+            MyRobot myRobot = new MyRobot();
+            moveFile(myRobot);
+            trash(myRobot);
+            goToBase(myRobot);
+        }
         return "redirect:/";
     }
 }
