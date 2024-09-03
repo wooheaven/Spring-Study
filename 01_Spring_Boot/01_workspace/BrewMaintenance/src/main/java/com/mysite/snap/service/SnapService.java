f30770388e2f6a60049f812fd74a612942092914
WooRung <wooheaven79@gmail.com>
2024-09-03 15:31:50 +0000

    #138-add-loop-for-snap-refresh-list ``` modified:   01_Spring_Boot/01_workspace/BrewMaintenance/src/main/java/com/mysite/snap/service/SnapService.java ```

--- 01_Spring_Boot/01_workspace/BrewMaintenance/src/main/java/com/mysite/snap/service/SnapService.java
+++ 01_Spring_Boot/01_workspace/BrewMaintenance/src/main/java/com/mysite/snap/service/SnapService.java
@@ -23,7 +23,7 @@ import java.util.List;
 public class SnapService {
     private static final String snapList = "sudo -S <<< \"12qwas\" snap list --all 2>&1 | sed -e 's/\\[sudo\\] password for woo: //' | xargs -rn6";
     private static final String snapRemove = "sudo -S <<< \"12qwas\" snap remove ";
-    private static final String snapRefreshList = "sudo -S <<< \"12qwas\" snap refresh --list 2>&1 | xargs -r";
+    private static final String snapRefreshList = "sudo -S <<< \"12qwas\" snap refresh --list 2>&1 | xargs -r ";
     private static final String snapRefresh = "sudo -S <<< \"12qwas\" snap refresh ";
     private static final String snapChanges = "sudo -S <<< \"12qwas\" snap changes | sed -e 's/\s\\{3,\\}/  /g' ";
     private final CommonService commonService;
@@ -59,24 +59,28 @@ public class SnapService {
             content += "\n";
         }
         content = content.replace("[sudo] password for woo: ", "");
+        content = content.replace("Name Version Rev Size Publisher Notes ", "");
         content = content.replaceAll("\n$", "");
 
         // save snap refresh list
-        SnapRefreshList snapRefreshList = new SnapRefreshList();
-        if (content.contains("All snaps up to date."))
+        SnapRefreshList snapRefreshList = null;
+        if (content.contains("All snaps up to date.")) {
+            snapRefreshList = new SnapRefreshList();
             snapRefreshList.setRefreshLog(content);
-        else if (content.contains("Name")) {
-            content = content.replace("Name Version Rev Size Publisher Notes ", "");
+        } else {
             String[] fields = content.split(" ");
-            if (fields.length == 6)
-                snapRefreshList.setName(fields[0]);
-            snapRefreshList.setVersion(fields[1]);
-            snapRefreshList.setRev(fields[2]);
-            snapRefreshList.setSize(fields[3]);
-            snapRefreshList.setPublisher(fields[4]);
-            snapRefreshList.setNotes(fields[5]);
+            int snapRefreshLineCount = fields.length / 6;
+            for (int i = 0; i < snapRefreshLineCount; i++) {
+                snapRefreshList = new SnapRefreshList();
+                snapRefreshList.setName(fields[i*6 + 0]);
+                snapRefreshList.setVersion(fields[i*6 + 1]);
+                snapRefreshList.setRev(fields[i*6 + 2]);
+                snapRefreshList.setSize(fields[i*6 + 3]);
+                snapRefreshList.setPublisher(fields[i*6 + 4]);
+                snapRefreshList.setNotes(fields[i*6 + 5]);
+                this.snapRefreshListRepository.save(snapRefreshList);
+            }
         }
-        this.snapRefreshListRepository.save(snapRefreshList);
     }
 
     public Page<SnapRefreshList> getSnapRefreshList(int page) {
