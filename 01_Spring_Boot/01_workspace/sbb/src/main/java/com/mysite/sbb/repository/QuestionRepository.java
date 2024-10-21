994b0ca162d60961403ae002df514a9a0475a4ba
WooRung <wooheaven79@gmail.com>
2024-10-21 13:17:25 +0000

    #150-add-repository ``` new file:   01_Spring_Boot/01_workspace/sbb/src/main/java/com/mysite/sbb/repository/AnswerRepository.java new file:   01_Spring_Boot/01_workspace/sbb/src/main/java/com/mysite/sbb/repository/QuestionRepository.java ```

---
+++ 01_Spring_Boot/01_workspace/sbb/src/main/java/com/mysite/sbb/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.mysite.sbb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mysite.sbb.entity.Question;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
