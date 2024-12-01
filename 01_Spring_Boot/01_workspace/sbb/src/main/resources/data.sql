2bb526ed4ffab295dd018235f04a4b76ac7a9eab
WooRung <wooheaven79@gmail.com>
2024-12-01 05:43:34 +0000

    #163-modify-data-sql-2 ``` modified:   01_Spring_Boot/01_workspace/sbb/src/main/resources/data.sql ```

--- 01_Spring_Boot/01_workspace/sbb/src/main/resources/data.sql
+++ 01_Spring_Boot/01_workspace/sbb/src/main/resources/data.sql
@@ -18,9 +18,9 @@ INSERT INTO dho_user (user_id, game_id, user_nm, create_date) VALUES
 
 -- TO_DO
 INSERT INTO to_do (todo_id,user_user_id,content,create_date) VALUES 
-(DEFAULT,'U11','수지 개인농장 100단위로 12,000원에 팔기',CURRENT_TIMESTAMP()),
-(DEFAULT,'U11','점토 개인농장 100단위로 12,500원에 팔기',CURRENT_TIMESTAMP()),
-(DEFAULT,'U23','소금구이 모두 5,600원에 팔기',CURRENT_TIMESTAMP()),
-(DEFAULT,'U23','다랑어 올리브 스테이크 모두 23,800원에 팔기',CURRENT_TIMESTAMP()) ;
-(DEFAULT,'U23','프프란 트리 모두 199,000원에 팔기',CURRENT_TIMESTAMP()) ;
-(DEFAULT,'U24','Sai_Fi_Wh3 개발 중',CURRENT_TIMESTAMP()) ;
+(DEFAULT,'U11','수지 개인농장 100단위로 12,000원에 팔기',    CURRENT_TIMESTAMP()),
+(DEFAULT,'U11','점토 개인농장 100단위로 12,500원에 팔기',    CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','소금구이 모두 5,600원에 팔기',               CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','다랑어 올리브 스테이크 모두 23,800원에 팔기',CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','프프란 트리 모두 199,000원에 팔기',          CURRENT_TIMESTAMP()),
+(DEFAULT,'U24','Sai_Fi_Wh3 개발 중',                         CURRENT_TIMESTAMP()) ;
