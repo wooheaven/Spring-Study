be0acd778e9bf30007469e250cfdedb23e7c16e0
unknown <wooheaven79@gmail.com>
2024-12-26 03:45:35 +0900

    #177-modify-data-sql-10 ``` modified:   01_Spring_Boot/01_workspace/sbb/src/main/resources/data.sql ```

--- 01_Spring_Boot/01_workspace/sbb/src/main/resources/data.sql
+++ 01_Spring_Boot/01_workspace/sbb/src/main/resources/data.sql
@@ -18,11 +18,11 @@ INSERT INTO dho_user (user_id, game_id, user_nm, create_date) VALUES
 
 -- DHO_LOGIN
 INSERT INTO dho_login (num, left_user_id, right_user_id) VALUES
-(1, 'U11', 'U24'),
-(2, 'U11', 'U23'),
+(1, 'U35', 'U23'),
+(2, 'U36', 'U23'),
 (3, 'U12', 'U23'),
-(4, 'U35', 'U23'),
-(5, 'U36', 'U23'),
+(4, 'U11', 'U23'),
+(5, 'U11', 'U24'),
 (6, 'U11', 'U23') ;
 UPDATE dho_login AS a
    SET a.left_game_id = (SELECT b.game_id FROM dho_user AS b WHERE b.user_id = a.left_user_id)
@@ -37,26 +37,28 @@ UPDATE dho_login AS a
 
 -- TO_DO
 INSERT INTO to_do (todo_id,user_user_id,content,create_date) VALUES 
-(DEFAULT,'U11','수지 개인농장 100단위로 12,000원에 팔기',               CURRENT_TIMESTAMP()),
-(DEFAULT,'U11','점토 개인농장 100단위로 12,500원에 팔기',               CURRENT_TIMESTAMP()),
-(DEFAULT,'U11','커민 갯수에 맞춰서 마늘, 코리안더를 U23 에게 전달하기', CURRENT_TIMESTAMP()),
-(DEFAULT,'U11','바질 83 을 U23 에게 전달하기',                          CURRENT_TIMESTAMP()),
-(DEFAULT,'U11','타임, 로즈마리 모두 U23 에게 전달하기',                 CURRENT_TIMESTAMP()),
-(DEFAULT,'U11','정어리 600 유지하기',                                   CURRENT_TIMESTAMP()),
-(DEFAULT,'U11','다랑어 50 단위로 U23 에게 전달하기',                    CURRENT_TIMESTAMP()),
-(DEFAULT,'U23','바질 83 을 버질리코 스파게티로 만들기',                 CURRENT_TIMESTAMP()),
-(DEFAULT,'U23','소금구이 모두 6,200원에 팔기',                          CURRENT_TIMESTAMP()),
-(DEFAULT,'U23','다랑어 올리브 스테이크 모두 27,000원에 팔기',           CURRENT_TIMESTAMP()),
-(DEFAULT,'U23','프프란 트리 모두 225,000원에 팔기',                     CURRENT_TIMESTAMP()),
-(DEFAULT,'U23','합계 Lv116 | 파티쉐로 전직하기',                        CURRENT_TIMESTAMP()),
-(DEFAULT,'U23','합계 Lv106 | 모험34 상인63 군인9',                      CURRENT_TIMESTAMP()),
-(DEFAULT,'U24','Sai_Fi_Wh3 개발 중',                                    CURRENT_TIMESTAMP()),
-(DEFAULT,'U24','U23 플랜트와 똑같이 만들기',                            CURRENT_TIMESTAMP()), 
-(DEFAULT,'U24','굴소스, 가람 마사라의 가격을 확인하기',                 CURRENT_TIMESTAMP()), 
-(DEFAULT,'U24','조선차, 육두구, 메이스의 가격을 확인하기',              CURRENT_TIMESTAMP()), 
-(DEFAULT,'U24','굴소스 가격이 좋으면 굴 짝수 모두 U11 에게 전달하기',   CURRENT_TIMESTAMP()), 
-(DEFAULT,'U24','타임 수량만큼 로즈마리를 U11 에게 전달하기',            CURRENT_TIMESTAMP()), 
-(DEFAULT,'U24','바질 83 을 U11에게 전달하기',                           CURRENT_TIMESTAMP()), 
-(DEFAULT,'U24','정어리 80 을 U11에게 전달하기',                         CURRENT_TIMESTAMP()), 
-(DEFAULT,'U24','다랑어 50 단위로 U11 에게 전달하기',                    CURRENT_TIMESTAMP()), 
-(DEFAULT,'U24','소금 50, 오레가노 50, 로즈마리 50 런던에서 판매하기',   CURRENT_TIMESTAMP()) ;
+(DEFAULT,'U11','수지 개인농장 100단위로 12,000원에 팔기',                CURRENT_TIMESTAMP()),
+(DEFAULT,'U11','점토 개인농장 100단위로 12,500원에 팔기',                CURRENT_TIMESTAMP()),
+(DEFAULT,'U11','커민 갯수에 맞춰서 마늘, 코리안더를 U23 에게 전달하기',  CURRENT_TIMESTAMP()),
+(DEFAULT,'U11','바질 83 을 U23 에게 전달하기',                           CURRENT_TIMESTAMP()),
+(DEFAULT,'U11','타임, 로즈마리 모두 U23 에게 전달하기',                  CURRENT_TIMESTAMP()),
+(DEFAULT,'U11','정어리 600 유지하기',                                    CURRENT_TIMESTAMP()),
+(DEFAULT,'U11','다랑어 50 단위로 U23 에게 전달하기',                     CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','바질 83 을 버질리코 스파게티로 만들기',                  CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','소금구이 모두 6,400원에 팔기',                           CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','다랑어 올리브 스테이크 모두 27,000원에 팔기',            CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','프프란 트리 모두 225,000원에 팔기',                      CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','합계 Lv116 | 파티쉐로 전직하기',                         CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','합계 Lv106 | 모험34 상인63 군인9',                       CURRENT_TIMESTAMP()),
+(DEFAULT,'U23','새우 허브마리 200 까지 달걀 닭 생산하기',                CURRENT_TIMESTAMP()),
+(DEFAULT,'U24','Sai_Fi_Wh3 개발 중',                                     CURRENT_TIMESTAMP()),
+(DEFAULT,'U24','U23 플랜트와 똑같이 만들기',                             CURRENT_TIMESTAMP()), 
+(DEFAULT,'U24','굴소스, 가람 마사라의 가격을 확인하기',                  CURRENT_TIMESTAMP()), 
+(DEFAULT,'U24','조선차, 육두구, 메이스, 동권총, 일본도 가격을 확인하기', CURRENT_TIMESTAMP()), 
+(DEFAULT,'U24','굴소스 가격이 좋으면 굴 짝수 모두 U11 에게 전달하기',    CURRENT_TIMESTAMP()), 
+(DEFAULT,'U24','타임 수량만큼 로즈마리를 U11 에게 전달하기',             CURRENT_TIMESTAMP()), 
+(DEFAULT,'U24','바질 83 을 U11에게 전달하기',                            CURRENT_TIMESTAMP()), 
+(DEFAULT,'U24','정어리 80 을 U11에게 전달하기',                          CURRENT_TIMESTAMP()), 
+(DEFAULT,'U24','다랑어 50 단위로 U11 에게 전달하기',                     CURRENT_TIMESTAMP()), 
+(DEFAULT,'U24','소금 50, 오레가노 50, 로즈마리 50 런던에서 판매하기',    CURRENT_TIMESTAMP()),
+(DEFAULT,'U35','선착장 5번 개발 중',                                     CURRENT_TIMESTAMP()) ;
