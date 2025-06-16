b7b9b4e2f68e59766464611a6083179efe225d3e
WooRung <wooheaven79@gmail.com>
2025-06-16 00:28:21 +0000

    #198-modify-and-delete-answer-by-author ``` modified:   01_Spring_Boot/01_workspace/sbb/src/main/java/com/mysite/sbb/answer/AnswerController.java new file:   01_Spring_Boot/01_workspace/sbb/src/main/java/com/mysite/sbb/answer/AnswerForm.java modified:   01_Spring_Boot/01_workspace/sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java modified:   01_Spring_Boot/01_workspace/sbb/src/main/resources/data.sql new file:   01_Spring_Boot/01_workspace/sbb/src/main/resources/templates/answer_form.html modified:   01_Spring_Boot/01_workspace/sbb/src/main/resources/templates/question_detail.html ```

--- 01_Spring_Boot/01_workspace/sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
+++ 01_Spring_Boot/01_workspace/sbb/src/main/java/com/mysite/sbb/answer/AnswerService.java
@@ -1,7 +1,9 @@
 package com.mysite.sbb.answer;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.user.SiteUser;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +24,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
